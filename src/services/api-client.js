1a7c75c015e314717d08897fb9b3768bda84b9b6
samiul haque <mdsamiulhaque682@gmail.com>
2026-03-06 11:50:53 +0600

    added vercel.json

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://pet-haven-kappa.vercel.app/api/"
+    baseURL: "https://pet-haven-kappa.vercel.app/api"
 })
  
 export default apiClient;
--- src/services/auth-api-client.js
+++ src/services/auth-api-client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApiClient = axios.create({
-  baseURL: "https://pet-haven-kappa.vercel.app/api/",
+  baseURL: "https://pet-haven-kappa.vercel.app/api",
 });
 
 export default authApiClient;
